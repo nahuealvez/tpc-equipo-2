2bbd96c308b384551561a6290bc6c0b8a20e51c7
Romero Nicolás <73563506+Watsoncurl@users.noreply.github.com>
2023-10-26 19:12:58 -0300

    Database
    Agregado,
    Esquema Medico

---
+++ TPC_equipo-2/Database/Security/Medico.sql
@@ -0,0 +1,2 @@
+﻿/****** Object:  Schema [Medico]    Script Date: 26/10/2023 18:53:00 ******/
+CREATE SCHEMA [Medico]
---
+++ TPC_equipo-2/Database/Security/Paciente.sql
@@ -0,0 +1,2 @@
+﻿/****** Object:  Schema [Paciente]    Script Date: 26/10/2023 18:53:00 ******/
+CREATE SCHEMA [Paciente]
