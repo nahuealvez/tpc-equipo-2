57339a228e82a4b66b67c6e8c914f1ea9173903b
Nahuel Alvez <nahue.alvez@gmail.com>
2023-10-29 21:57:28 -0300

    Creacion de clase de acceso a datos

---
+++ TPC_equipo-2/Negocio/AccesoDatos.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            // Conexión Nahue
+            conexion = new SqlConnection("server=localhost; database=ConsultorioMedico; integrated security=false; User ID=sa; Password=Outatimerun1985");
+            // Conexión Iña
+            // conexion = new SqlConnection("server=localhost; database=ConsultorioMedico; integrated security=false; User ID=sa; Password=Gloria3279");
+            // Conexión Nico
+            //conexion = new SqlConnection("server=localhost; database=ConsultorioMedico; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void EjecutarAccion()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+            {
+                lector.Close();
+            }
+            conexion.Close();
+        }
+
+        public void SetearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+    }
+}
