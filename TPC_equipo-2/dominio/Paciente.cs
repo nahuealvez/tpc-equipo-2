c5f4ca52096fe74c125897263f415843cb4bb164
Nahuel Alvez <nahue.alvez@gmail.com>
2023-11-09 00:10:12 -0300

    Se agrega clase Paciente

---
+++ TPC_equipo-2/dominio/Paciente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Paciente
+    {
+        public int idPaciente {  get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public int Dni {  get; set; }
+        public string Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Mail {  get; set; }
+        public string Telefono { get; set; }
+        public string Cobertura { get; set; }
+        public string NroCredencial { get; set; }
+    }
+}
