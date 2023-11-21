3ed81da7bc8cb38209a27794ea0327b485cab490
iniakig <iniakigaldos@gmail.com>
2023-11-21 13:19:37 -0300

    Se agregaron metodos a JornadaNegocio

---
+++ TPC_equipo-2/Negocio/JornadaNegocio.cs
@@ -0,0 +1,121 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class JornadaNegocio
+    {
+        public List<Jornada> Listar()
+        {
+            List<Jornada> lista = new List<Jornada>();
+            List<Usuario> especialistas = new List<Usuario>();
+            List<Especialidad> especialidades = new List<Especialidad>();
+            UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+            EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
+
+            especialistas = usuarioNegocio.ListarEspecialistas();
+            especialidades = especialidadNegocio.Listar();
+
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT * FROM Usuario.Jornada");
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Jornada aux = new Jornada();
+                    aux.IdJornada = (int)datos.Lector["IdJornada"];
+                    aux.Especialidad = especialidades.Find(x => x.Id == (int)datos.Lector["IdEspecialidad"]);
+                    aux.Especialista = especialistas.Find(x => x.IdUsuario == (int)datos.Lector["IdEspecialista"]);
+                    aux.DiaSemana = (string)datos.Lector["DiaSemana"];
+                    aux.HoraInicio = (TimeSpan)datos.Lector["HoraInicio"];
+                    aux.HoraFin = (TimeSpan)datos.Lector["HoraFin"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+        public List<Jornada> ListarXEspecialista(Usuario Especialista)
+        {
+            List<Jornada> lista = new List<Jornada>();
+            List<Especialidad> especialidades = new List<Especialidad>();
+            EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
+
+            especialidades = especialidadNegocio.Listar();
+
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("SELECT * FROM Usuario.Jornada WHERE IdEspecialista = @Id");
+                datos.SetearParametro("@Id", Especialista.IdUsuario);
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Jornada aux = new Jornada();
+                    aux.IdJornada = (int)datos.Lector["IdJornada"];
+                    aux.Especialidad = especialidades.Find(x => x.Id == (int)datos.Lector["IdEspecialidad"]);
+                    aux.Especialista = Especialista;
+                    aux.DiaSemana = (string)datos.Lector["DiaSemana"];
+                    aux.HoraInicio = (TimeSpan)datos.Lector["HoraInicio"];
+                    aux.HoraFin = (TimeSpan)datos.Lector["HoraFin"];
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        public void Agregar (Jornada nuevaJornada)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("EXEC [Usuario].[sp_InsJornada] @IdEspecialidad, @IdEspecialista, @DiaSemana, @HoraInicio, @HoraFin");
+                datos.SetearParametro("@IdEspecialidad", nuevaJornada.Especialidad.Id);
+                datos.SetearParametro("@IdEspecialista", nuevaJornada.Especialista.IdUsuario);
+                datos.SetearParametro("@DiaSemana", nuevaJornada.DiaSemana);
+                datos.SetearParametro("@HoraInicio", nuevaJornada.HoraInicio);
+                datos.SetearParametro("@HoraFin", nuevaJornada.HoraFin);
+                datos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
+        
+    }
+}
--- TPC_equipo-2/dominio/Usuario.cs
+++ TPC_equipo-2/dominio/Usuario.cs
@@ -9,7 +9,6 @@ namespace dominio
     public class Usuario : Persona
     {
         public int IdUsuario { get; set; }
-
         public string UsuarioReg { get; set; }
         public string Password { get; set; }
         public int Perfil { get; set; }
