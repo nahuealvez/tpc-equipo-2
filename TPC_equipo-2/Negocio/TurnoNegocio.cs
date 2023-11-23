34c61b300e8e1e64599b257056a5053fea946900
iniakig <iniakigaldos@gmail.com>
2023-11-23 18:03:03 -0300

    Se ajusto la consulta para listar los turnos asignados

--- TPC_equipo-2/Negocio/TurnoNegocio.cs
+++ TPC_equipo-2/Negocio/TurnoNegocio.cs
@@ -12,31 +12,42 @@ namespace Negocio
         public List<Turno> Listar()
         {
             List<Turno> lista = new List<Turno>();
+            List<Usuario> especialistas = new List<Usuario>();
+            List<Especialidad> especialidades = new List<Especialidad>();
+            UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+            EspecialidadNegocio especialidadNegocio = new EspecialidadNegocio();
+
+            especialistas = usuarioNegocio.ListarEspecialistas();
+            especialidades = especialidadNegocio.Listar();
+
             AccesoDatos datos = new AccesoDatos();
 
             try
             {
-                datos.SetearConsulta("SELECT A.IdTurno, A.FechaHoraTurno, A.MotivoConsulta, A.Diagnostico, B.Dni AS DniPaciente, B.Apellido AS ApellidoPaciente, B.Nombre AS NombrePaciente , C.Apellido AS ApellidoEspecialista, C.Nombre AS NombreEspecialista, D.Especialidad, E.NombreEstado FROM Especialidad.Turno AS A LEFT JOIN Usuario.Paciente AS B ON A.IdPaciente = B.IdPaciente LEFT JOIN Usuario.Usuario AS C ON A.IdEspecialista = C.IdUsuario LEFT JOIN Especialidad.Especialidad AS D ON A.IdEspecialidad = D.IdEspecialidad LEFT JOIN Especialidad.EstadoTurno AS E ON A.IdEstadoTurno = E.IdEstadoTurno");
+                //datos.SetearConsulta("SELECT A.IdTurno, A.FechaHoraTurno, A.MotivoConsulta, A.Diagnostico, B.Dni AS DniPaciente, B.Apellido AS ApellidoPaciente, B.Nombre AS NombrePaciente , C.Apellido AS ApellidoEspecialista, C.Nombre AS NombreEspecialista, D.Especialidad, E.NombreEstado FROM Especialidad.Turno AS A LEFT JOIN Usuario.Paciente AS B ON A.IdPaciente = B.IdPaciente LEFT JOIN Usuario.Usuario AS C ON A.IdEspecialista = C.IdUsuario LEFT JOIN Especialidad.Especialidad AS D ON A.IdEspecialidad = D.IdEspecialidad LEFT JOIN Especialidad.EstadoTurno AS E ON A.IdEstadoTurno = E.IdEstadoTurno");
+                datos.SetearConsulta("SELECT * FROM Especialidad.Turno");
                 datos.EjecutarLectura();
 
                 while (datos.Lector.Read())
                 {
                     Turno aux = new Turno();
                     aux.IdTurno = (int)datos.Lector["IdTurno"];
-                    aux.Paciente = new Paciente();
-                    aux.Paciente.Dni = (Int32)datos.Lector["DniPaciente"];
-                    aux.Paciente.Apellido = (string)datos.Lector["ApellidoPaciente"];
-                    aux.Paciente.Nombre = (string)datos.Lector["NombrePaciente"];
-                    aux.Usuario = new Usuario();
-                    aux.Usuario.Apellido = (string)datos.Lector["ApellidoEspecialista"];
-                    aux.Usuario.Nombre = (string)datos.Lector["NombreEspecialista"];
-                    aux.Especialidad = new Especialidad();
-                    aux.Especialidad.Descripcion = (string)datos.Lector["Especialidad"];
+                    //aux.Paciente = new Paciente();
+                    //aux.Paciente.Dni = (Int32)datos.Lector["DniPaciente"];
+                    //aux.Paciente.Apellido = (string)datos.Lector["ApellidoPaciente"];
+                    //aux.Paciente.Nombre = (string)datos.Lector["NombrePaciente"];
+                    aux.Usuario = especialistas.Find(x => x.IdUsuario == (int)datos.Lector["IdEspecialista"]);
+                    //aux.Usuario = new Usuario();
+                    //aux.Usuario.Apellido = (string)datos.Lector["ApellidoEspecialista"];
+                    //aux.Usuario.Nombre = (string)datos.Lector["NombreEspecialista"];
+                    aux.Especialidad = especialidades.Find(x => x.Id == (int)datos.Lector["IdEspecialidad"]);
+                    //aux.Especialidad = new Especialidad();
+                    //aux.Especialidad.Descripcion = (string)datos.Lector["Especialidad"];
                     aux.FechaHora = (DateTime)datos.Lector["FechaHoraTurno"];
                     aux.MotivoConsulta = (string)datos.Lector["MotivoConsulta"];
                     aux.Diagnostico = (string)datos.Lector["Diagnostico"];
-                    aux.Estado = new EstadoTurno();
-                    aux.Estado.Descripcion = (string)datos.Lector["NombreEstado"];
+                    //aux.Estado = new EstadoTurno();
+                    //aux.Estado.Descripcion = (string)datos.Lector["NombreEstado"];
                     lista.Add(aux);
                 }
 
